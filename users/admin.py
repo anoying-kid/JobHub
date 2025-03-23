b71ca53cbbf701f25f80a4aebb5c02812d0b0620
anoying-kid <yugrana853@gmail.com>
2025-03-23 22:48:41 +0530

    Registered it to the admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, JobSeekerProfile, EmployerProfile
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'role', 'is_staff']
+    fieldsets = UserAdmin.fieldsets + (
+        ('Role', {'fields': ('role',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Role', {'fields': ('role',)}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(JobSeekerProfile)
+admin.site.register(EmployerProfile)
